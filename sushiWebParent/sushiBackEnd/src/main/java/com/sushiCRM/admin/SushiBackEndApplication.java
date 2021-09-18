c6d2c2bec2ac0e26238a00a2e4ee7077dd4a4ccd
Brian Hoots <thehoots26@gmail.com>
2021-09-18 03:26:37 -0500

    Kind of an initial commit

---
+++ sushiWebParent/sushiBackEnd/src/main/java/com/sushiCRM/admin/SushiBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sushiCRM.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiBackEndApplication.class, args);
+	}
+
+}
---
+++ sushiWebParent/sushiFrontEnd/src/main/java/com/sushiCRM/website/SushiFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.sushiCRM.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiFrontEndApplication.class, args);
+	}
+
+}
